810dbd23b70811d4a8fa4cfc0318871aad940e7c
Jefferson <jefferson.rios.caro@gmail.com>
2024-05-23 15:15:53 -0400

    chore: Add an action to test

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@ import { start, stop } from "./lib/ec2Handler.js";
 async function run() {
   try {
     const instances_id = JSON.parse(core.getInput('instances_id', {required: true}))
-    const action = (core.getInput('action', {required: true})).toLowerCase
+    const action = core.getInput('action', {required: true})
 
-    if (action === "start") {
+    if (action.toLowerCase() === "start") {
       await start(instances_id);
       return
     }
 
-    if (action === "stop") {
+    if (action.toLowerCase() === "stop") {
       await stop(instances_id);
       return
     }
 
-
+    core.setFailed("Invalid action. Please provide either 'start' or 'stop' as action")
   } catch (err) {
     core.setFailed(err);
   }
